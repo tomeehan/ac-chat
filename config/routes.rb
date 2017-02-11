b75452a65dbf4b852a9ee2dd7630600e964ba4d2
tomeehan <tom.meehan@me.com>
2017-02-11 20:04:41 +0000

    action cable route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :chat_rooms, only: [:new, :create, :show, :index]
   root 'chat_rooms#index'
+
+  mount ActionCable.server => '/cable'
 end
