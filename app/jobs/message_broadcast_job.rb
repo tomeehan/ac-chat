d01edf3f70a5a3b5916d0d9f4d0b6d79bef6e558
tomeehan <tom.meehan@me.com>
2017-02-11 21:26:04 +0000

    render_message

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -1,13 +1,14 @@
 class MessageBroadcastJob < ApplicationJob
-	queue_as :default
+  queue_as :default
 
-	def perform(message)
-		ActionCable.server.broadcast "chat_rooms_#{messsage.chat_room.id}_channel", message: 'MESSAGE_HTML'
-	end
+  def perform(message)
+    ActionCable.server.broadcast "chat_rooms_#{message.chat_room.id}_channel",
+                                 message: render_message(message)
+  end
 
-	private
+  private
 
-	def render_message(message)
-		MessagesController.render partial: 'messages/message', locals: {message: message}
-	end
+  def render_message(message)
+    MessagesController.render partial: 'messages/message', locals: {message: message}
+  end
 end
