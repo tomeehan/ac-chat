634a7394f749eba0f1ad929524e81e342b6592d7
tomeehan <tom.meehan@me.com>
2017-02-11 21:18:36 +0000

    clean up indentation

--- app/channels/chat_rooms_channel.rb
+++ app/channels/chat_rooms_channel.rb
@@ -1,14 +1,13 @@
 class ChatRoomsChannel < ApplicationCable::Channel
-	def subscribed
-		stream_from "chat_rooms_#{params['chat_room_id']}_channel"
-	end
+  def subscribed
+    stream_from "chat_rooms_#{params['chat_room_id']}_channel"
+  end
 
-	def unsubscribed
-		# cleanup when channel is unsubscribed
-	end
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
 
-	def send_message(data)
-		# process data send from page
-		current_user.messages.create!(body: data['message'], chat_room_id: data['chat_room_id'])
-	end
+  def send_message(data)
+    current_user.messages.create!(body: data['message'], chat_room_id: data['chat_room_id'])
+  end
 end
